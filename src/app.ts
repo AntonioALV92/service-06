941be682c35b28349f88fa806a6ae2265011a1e6
Luis <luisegr149@gmail.com>
2019-05-15 13:09:16 -0500

    :loud_sound: Agregados logs de request y response

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import packageJson from '../package.json';
 import { RegistroInicialController } from './controllers';
 import { ErrorHandler } from './middlewares/ErrorHandler';
 import { JSONSchema } from './middlewares/JSONSchemaValidator';
+import { LoggerRequest } from './middlewares/LoggerRequest';
 // import { requests } from 'banca-movil-schemas/schemas';
 import { Mapper } from './middlewares/Mapper';
 
@@ -21,6 +22,7 @@ app.use(bodyParser.json());
 
 const schema = schemas.requests.RegistroInicialRequest;
 app.use('/general', [
+    LoggerRequest,
     JSONSchema(schema), // Valida petición contra JSON Schema
     Mapper,  // Mapea campos de petición a como se requiere por banxico
     RegistroInicialController, // Manda la petición a banxico
--- src/controllers/registroInicial.controller.ts
+++ src/controllers/registroInicial.controller.ts
@@ -3,20 +3,33 @@ import { Request, Response, Router } from 'express';
 import got from 'got';
 import routes from '../../routes.json';
 import { mapear } from '../middlewares/Mapper';
-
+const LoggerColors = require('logger-colors');
 const router: Router = Router();
+const logger = new LoggerColors();
+
+const separador = '----------------------------------------------------';
 
 // La ruta depende de donde se monte este controller en server.ts, en
 // este caso este controller está en /general, por lo tanto esta
 // ruta está ligada a /general/registroInicial
 // Usando 'wrap' no tenemos que hacer try-catch para los métodos async
 router.post("/registroInicial", wrap(async (req: Request, res: Response) => {
+
     const response = await got.post(routes.registroInicial, {
         body: "d=" + JSON.stringify(req.body),
         headers: {
             "Content-Type": "text/plain",
         },
     });
+
+    logger.magenta('RESPONSE', true);
+    logger.magenta(`STATUS: [${logger.c_white}${response.statusCode}${logger.c_magenta}]`, false);
+    logger.magenta('');
+    // logger.magenta('HEADERS BANXICO');
+    // logger.magenta(JSON.stringify(response.headers, LoggerColors.smallJSON), false);
+    // logger.magenta('');
+    // logger.magenta('BODY BANXICO:');
+    // logger.magenta(response.body);
 
     try {
         const respuesta = JSON.parse(response.body);
@@ -25,9 +38,16 @@ router.post("/registroInicial", wrap(async (req: Request, res: Response) => {
             edoPet: 'estadoPeticion',
             gId: 'googleId',
         });
+
+        logger.magenta('BODY');
+        logger.magenta(JSON.stringify(respuestaMapeada), false);
+
         res.status(200).send(respuestaMapeada);
+
+        logger.info(separador);
+
     } catch (e) {
-        throw new Error('Error al procesar respuesta: "' + response.body + '"');
+        logger.error('Error al procesar respuesta: "' + response.body + '"');
     }
 }));
 
